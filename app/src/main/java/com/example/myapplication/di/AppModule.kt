1711045cec1eb7d285fcc9f4b9f1fb0a346a804d
Bruno Dewes <brunodewes@alunos.utfpr.edu.br>
2023-09-06 03:03:11 -0300

    changing to compose

--- app/src/main/java/com/example/myapplication/di/AppModule.kt
+++ app/src/main/java/com/example/myapplication/di/AppModule.kt
@@ -1,12 +1,20 @@
 package com.example.myapplication.di
 
+import com.example.myapplication.repository.TaskRepository
+import com.example.myapplication.repository.TaskRepositoryImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideTaskRepository(): TaskRepository {
+        return TaskRepositoryImpl()
+    }
 }
